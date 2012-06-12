0bfecc85c3252bc64e681323e18bd45d395baa99
Aleksey V Zapparov <ixti@member.fsf.org>
2012-06-12 12:11:43 +0200

    Refactor import_tree()

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function find_files(pathname) {
   var files = [];
 
   fs.readdirSync(pathname).forEach(function (filename) {
-    filename = path.join(pathname, filename),
+    filename = path.join(pathname, filename);
 
     if (fs.statSync(filename).isDirectory()) {
       files = files.concat(find_files(filename));
@@ -28,6 +28,28 @@ function find_files(pathname) {
 }
 
 
+function import_tree(expr) {
+  var block = this.currentBlock,
+      vals  = [stylus.nodes['null']];
+
+  pathname = path.resolve(path.dirname(this.filename), expr.val);
+
+  find_files(pathname).forEach(function (file) {
+    var expr = new stylus.nodes.Expression(),
+        node = new stylus.nodes.String(file),
+        body;
+
+    expr.push(node);
+
+    body = this.visitImport(new stylus.nodes.Import(expr));
+    vals = vals.concat(body.nodes);
+  }, this);
+
+  this.mixin(vals, block);
+  return vals;
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 
@@ -38,27 +60,8 @@ var source    = fs.readFileSync(filename, 'utf8');
 ////////////////////////////////////////////////////////////////////////////////
 
 
-stylus(source)
-  .define('import_tree', function (pathname) {
-    var block = this.currentBlock,
-        vals  = [stylus.nodes['null']];
-
-    pathname = path.resolve(path.dirname(filename), pathname.val);
-
-    find_files(pathname).forEach(function (file) {
-      var expr = new stylus.nodes.Expression(),
-          node = new stylus.nodes.String(file),
-          body;
-
-      expr.push(node);
-
-      body = this.visitImport(new stylus.nodes.Import(expr));
-      vals = vals.concat(body.nodes);
-    }, this);
-
-    this.mixin(vals, block);
-    return vals;
-  })
+stylus(source, {filename: filename})
+  .define('import_tree', import_tree)
   .render(function (err, css) {
     console.log(css);
   });
